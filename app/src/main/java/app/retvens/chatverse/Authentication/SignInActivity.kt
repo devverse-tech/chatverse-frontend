93f00dbb73d997bccfd0fcfeeb01423b96466574
DELL <prasc641133@gmail.com>
2023-06-24 20:19:23 +0530

    first commit

---
+++ app/src/main/java/app/retvens/chatverse/Authentication/SignInActivity.kt
@@ -0,0 +1,12 @@
+package app.retvens.chatverse.Authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.chatverse.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
